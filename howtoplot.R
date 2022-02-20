b0a7aaff0411e271579e6a5a8283044c94a2054f
noa <noanonkes@gmail.com>
2022-02-20 16:38:24 +0100

    Updated plots and index page

--- howtoplot.R
+++ howtoplot.R
@@ -1,12 +1,16 @@
 playlist90 <- get_playlist("37i9dQZF1DX186v583rmzp")
-tracks90 <- get_playlist_tracks(playlist90)
 tracks90 <- get_playlist_tracks("37i9dQZF1DX186v583rmzp")
 features90 <- get_track_audio_features(tracks90$track.id)
+
 tracks10 <- get_playlist_tracks("37i9dQZF1DWSMW5YBCZisa")
 features10 <- get_track_audio_features(tracks10$track.id)
 
+tracks15 <- get_playlist_tracks("37i9dQZF1DXcqWbpeXswkc")
+features15 <- get_track_audio_features(tracks15$track.id)
+
 features10$Year <- 2010
 features90$Year <- 1990
+features15$Year <- 2015
 
 features <- rbind(features90, features10)
 
